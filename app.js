b2bab6f2100acc8d0ba2551c943eb4bb13d4eac5
Edita Marija <edimcdowl@yahoo.com>
2020-06-29 13:51:39 +0200

    delete item from UI

--- app.js
+++ app.js
@@ -171,6 +171,12 @@ var UIController = (function() {
             
         },
         
+        deleteListItem: function(selectorID) {
+            
+                var el = document.getElementById(selectorID);
+                el.parentNode.removeChild(el);
+        },
+        
         clearFields: function() {
             var fields, fieldsArr;
             
@@ -286,9 +292,11 @@ var controller = (function(budgetCtrl, UICtrl) {
             
             // 2. delete the item from the UI
             
+            UICtrl.deleteListItem(itemID);
+            
             // 3. update and show the new budget
             
-            
+            updateBudget();
         }
     };
     
